919292d4cab8b184dd83607efc1e45a8d0a54692
kuz1 <jan.kuzel@vf.cz>
2015-06-10 22:06:29 +0200

    +toolchain example

---
+++ C/Toolchain/main.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+//#include <stdlib.h>
+
+#define OK 2
+#define MSG "Toolchain example"
+
+int main(void)
+{
+	puts(MSG);
+	return OK;
+	//printf("Toolchain example.");
+	//return EXIT_SUCCESS;
+}
---
+++ C/Toolchain/main.s
@@ -0,0 +1,26 @@
+	.file	"main.c"
+	.section	.rodata
+.LC0:
+	.string	"Toolchain example"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	movl	$2, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu/Linaro 4.8.1-10ubuntu9) 4.8.1"
+	.section	.note.GNU-stack,"",@progbits
