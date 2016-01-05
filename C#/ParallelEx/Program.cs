22f3a52c8c059e8ea70ce2cc4392625c28d16b05
kuz1 <jan.kuzel@vf.cz>
2016-01-05 10:52:07 +0100

    A System.Threading.Tasks.Parallel example

---
+++ C#/ParallelEx/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelEx
+{
+    class Program
+    {
+        static void Body()
+        {
+            // Print for index and thread id
+            Parallel.For(0, 1000, (i) =>
+            {
+                Console.WriteLine($"{i}:{Thread.CurrentThread.ManagedThreadId}");
+            });
+
+            // Waiting until finish each action - something like barrier (!)
+        }
+
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+
+            Body();
+
+            Console.WriteLine($"{Thread.CurrentThread.Name}");
+            Console.ReadLine();
+        }
+    }
+}
