b9556c9be0315b4fb81acad6ef4d9ba27bff881e
kuz1 <jan.kuzel@vf.cz>
2016-01-06 10:03:04 +0100

    A infinity number generator example

---
+++ C#/InfiniteGen/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace InfiniteGen
+{
+    class Program
+    {
+        static IEnumerable<int> GetNumber()
+        {
+            int count = 0;
+            while (true)
+            {
+                yield return count;
+                count++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Infinite loop (!)
+            //var items = GetNumber();
+
+            // items = [1000..1100]
+            var items = GetNumber().Skip(1000).Take(101);
+            foreach (var i in items)
+                WriteLine(i);
+            ReadLine();
+        }
+    }
+}
