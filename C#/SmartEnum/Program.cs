f571a756dab657026084e7f9dfd74ba6b76cfa4e
Kuz1 <jan.kuzel@vf.cz>
2016-02-02 15:11:24 +0100

    A smart enum design pattern example

---
+++ C#/SmartEnum/Program.cs
@@ -0,0 +1,25 @@
+﻿using SmartEnum.src.dp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SmartEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"{Shape.Circle.Name}.ToString()");
+            WriteLine($"  {Shape.Circle.ToString()}");
+
+            // Comparison between values works out of the box as well. Circles are equal to Circles, but not to Rectangles.
+            WriteLine(Shape.Circle == Shape.Rectangle);
+            WriteLine(Shape.Circle == Shape.Circle);
+
+            ReadLine();
+        }
+    }
+}
---
+++ C#/SmartEnum/src/dp/Shape.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Source: http://blog.falafel.com/introducing-type-safe-enum-pattern/
+namespace SmartEnum.src.dp
+{
+    /*
+     * Classic way
+     */
+    //public enum Shape
+    //{
+    //    Circle,
+    //    Square,
+    //    Rectangle
+    //}
+
+    /*
+     * Smart way
+     *
+     * The class is declared sealed to prevent inheritance.
+     */
+    public sealed class Shape
+    {
+        // Any available fields on a Shape instance are public readonly fields.
+        public readonly String Name;
+        public readonly String Description;
+
+        // Every kind of Shape desired is declared as a public static readonly field.
+        public static readonly Shape Circle;
+        public static readonly Shape Square;
+        public static readonly Shape Rectangle;
+
+        static Shape()
+        {
+            Circle = new Shape("Circle", "first smart enum shape item");
+            Square = new Shape("Circle", "second smart enum shape item");
+            Rectangle = new Shape("Third", "third smart enum shape item");
+        }
+
+        // The constructor is made private since all Shapes are declared in the class itself.
+        private Shape(string name, string desctiption)
+        {
+            Name = name;
+            Description = desctiption;
+        }
+
+        // Since this is simply a C# class, developers can define any methods they want, override ToString(), and easily unit test any code in this class.
+        public override string ToString()
+        {
+            return $"Smart shape {Name} ({Description})";
+        }
+    }
+}
