7ec2a1c644739bad0ee3b99af9b8b306df9d1973
Kuz1 <jan.kuzel@vf.cz>
2016-02-04 14:47:18 +0100

    A Custom property in custom UserContol binding example

---
+++ C#/USBinding/ModelObject.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace USBinding
+{
+    public class ModelObject : INotifyPropertyChanged
+    {
+        private int _shoesize;
+
+        public int Shoesize
+        {
+            get { return _shoesize; }
+            set
+            {
+                _shoesize = value;
+                OnPropertyChanged("Shoesize");
+            }
+        }
+
+        private double _height;
+
+        public double Height
+        {
+            get { return _height; }
+            set
+            {
+                _height = value;
+                OnPropertyChanged("Height");
+            }
+        }
+
+        private double _sliderValue;
+
+        public double SliderValue
+        {
+            get { return _sliderValue; }
+            set
+            {
+                _sliderValue = value;
+                OnPropertyChanged("SliderValue");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
