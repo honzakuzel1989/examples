961e46b4a210215a2aee25d3178d4707e6865434
Kuz1 <jan.kuzel@vf.cz>
2016-03-07 10:14:38 +0100

    A Addition null trap example..

---
+++ C#/AdditionNullTrap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionNullTrap
+{
+    class SmartNumber
+    {
+        public int Y = 42;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Maybe expected n (10 in this case), but result is 0
+            Console.WriteLine(Add(10, null));
+
+            Console.ReadLine();
+        }
+
+        static int Add(int n, SmartNumber sn)
+        {
+            // if sn is null, result is always 0, because n + null (! int + null / numeric + null) is null and null ?? 0 is 0
+            return n + sn?.Y ?? 0;
+            // you have to use brackets
+            //return n + (sn?.Y ?? 0);
+        }
+    }
+}
