5a36d84992054a915ec0eb80670c5fc2366ab5c5
Kuz1 <jan.kuzel@vf.cz>
2016-03-21 09:35:40 +0100

    A Dispose call example

---
+++ C#/DisposeCall/CustomStreamReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposeCall
+{
+    class CustomStreamReader : StreamReader
+    {
+        public CustomStreamReader(string path) : base(path)
+        {
+            Console.WriteLine("Create CustomStreamReader");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            Console.WriteLine("Dispose CustomStreamReader");
+        }
+    }
+}
---
+++ C#/DisposeCall/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposeCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputContent = string.Empty;
+
+            // WRONG WAY
+
+            //using (DisposableClass dc = new DisposableClass())
+            //{
+            //    // Object Disposed Exception (!)
+            //    inputContent = dc.GetDisposedCustomStream().ReadToEnd();
+            //}
+
+            // GOOD WAY
+
+            using (DisposableClass dc = new DisposableClass())
+            {
+                using (CustomStreamReader cr = dc.GetCustomStream())
+                {
+                    inputContent = cr.ReadToEnd();
+                }
+            }
+
+            Console.WriteLine(inputContent);
+            Console.ReadLine();
+        }
+    }
+
+    class DisposableClass : IDisposable
+    {
+        public DisposableClass()
+        {
+            Console.WriteLine("Create DisposableClass");
+        }
+
+        public CustomStreamReader GetDisposedCustomStream()
+        {
+            using (CustomStreamReader sr = new CustomStreamReader("Input.txt"))
+            {
+                return sr;
+            }   // Dispose (!)
+        }
+
+        public CustomStreamReader GetCustomStream()
+        {
+            CustomStreamReader sr = new CustomStreamReader("Input.txt");
+            {
+                return sr;
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose DisposableClass");
+        }
+    }
+}
