483ade295e73fba88c5f566f595f986abb7b34f8
Kuz1 <jan.kuzel@vf.cz>
2016-04-08 12:47:28 +0200

    A protected internal access example

---
+++ C#/Library/C.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class C
+    {
+        // (!!) accessible in inherited classes and in this library
+        protected internal int X => 42;
+
+        public C()
+        {
+
+        }
+    }
+}
---
+++ C#/ProtectedInternal/Program.cs
@@ -0,0 +1,45 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Source: stackoverflow.com/questions/585859
+// msdn.microsoft.com/cs-cz/library/wxh6fsc7.aspx
+// protected internal: Access is limited to the current assembly OR types derived from the containing class.
+
+namespace ProtectedInternal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C c = new C();
+            //not accessible
+            //c.X
+
+            D d = new D();
+            //not accessible
+            //d.X
+
+            //accsessible using wrapper
+            Console.WriteLine(d.XX());
+
+            Console.ReadLine();
+        }
+    }
+
+    class D : C
+    {
+        public D() : base()
+        {
+
+        }
+
+        public int XX()
+        {
+            return X;
+        }
+    }
+}
