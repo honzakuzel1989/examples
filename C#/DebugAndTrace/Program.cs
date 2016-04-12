1f58e3e8acd00ca612d1dfce1f39e0aaadcd6d19
Kuz1 <jan.kuzel@vf.cz>
2016-04-12 10:31:34 +0200

    A Trace and Debug logging little example.

---
+++ C#/DebugAndTrace/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugAndTrace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // effective only if DEBUG flag is defined
+            // defined in Debug configuration (by default)
+            Console.WriteLine("Debug.WriteLine:");
+            Debug.WriteLine(nameof(Main));
+
+            // effective only if TRACE flag is defined
+            // defined in Debug and Release configuration (by default)
+            Console.WriteLine("Trace.WriteLine:");
+            Trace.WriteLine(nameof(Main));
+
+            Console.ReadLine();
+        }
+    }
+}
