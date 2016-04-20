3a5db448ace31e221cbbfe35e82eb0e7e22cb96f
Kuz1 <jan.kuzel@vf.cz>
2016-04-20 15:46:47 +0200

    A base view with palette of colors

---
+++ C#/ColorsInWrappedListBox/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorsInWrappedListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class ColorData
+        {
+            public string Name { get; set; }
+            public Brush Color { get; set; }
+            public string Code { get; set; }
+        }
+
+        public IEnumerable<ColorData> ColorsData { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            ColorsData = GetColors();
+        }
+
+        private IEnumerable<ColorData> GetColors()
+        {
+            IList<ColorData> colors = new List<ColorData>();
+
+            PropertyInfo[] brushes = typeof(Brushes).GetProperties();
+
+            foreach (var pi in brushes)
+            {
+                Brush b = (Brush)pi.GetValue(this);
+                if (b != null)
+                    colors.Add(new ColorData { Name = $"{pi.Name}", Code = $"x", Color = b});
+            }
+
+            return colors;
+        }
+    }
+}
