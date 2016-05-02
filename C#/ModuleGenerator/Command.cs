307484e013dad55bc9823dd005df0b46542f1712
Kuz1 <jan.kuzel@vf.cz>
2016-05-02 14:20:49 +0200

    Add command

---
+++ C#/ModuleGenerator/Command.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ModuleGenerator
+{
+    class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        private Action<object> methodToExecute;
+        private Func<object, bool> canExecuteEvaluator;
+        public Command(Action<object> methodToExecute, Func<object, bool> canExecuteEvaluator)
+        {
+            this.methodToExecute = methodToExecute;
+            this.canExecuteEvaluator = canExecuteEvaluator;
+        }
+        public Command(Action<object> methodToExecute)
+            : this(methodToExecute, null)
+        {
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecuteEvaluator == null)
+            {
+                return true;
+            }
+            else
+            {
+                bool result = this.canExecuteEvaluator.Invoke(parameter);
+                return result;
+            }
+        }
+        public void Execute(object parameter)
+        {
+            this.methodToExecute.Invoke(parameter);
+        }
+    }
+}
--- C#/ModuleGenerator/MainWindowModel.cs
+++ C#/ModuleGenerator/MainWindowModel.cs
@@ -8,5 +8,6 @@ namespace ModuleGenerator
 {
     class MainWindowModel : IModel
     {
+
     }
 }
