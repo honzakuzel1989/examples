be6d9772f1675d06e841a1a725eb4521c80fc0dc
kuz1 <jan.kuzel@vf.cz>
2016-06-17 15:37:51 +0200

    A Calculator example :)

---
+++ C#/Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using NCalc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(">Type expression or quit and press [Enter].");
+
+            while (true)
+            {
+                Write(">");
+                string input = ReadLine();
+                input = input.Trim();
+
+                if (input == "quit")
+                    break;
+
+                Expression exp = new Expression(input);
+                Write("<");
+                try { WriteLine(exp.Evaluate()); }
+                catch (Exception ex) { WriteLine(ex.Message); }
+            }
+        }
+    }
+}
