f2f65ed7cecd3bf950b85c2bc344ad027583e981
kuz1 <jan.kuzel@vf.cz>
2016-07-21 14:15:49 +0200

    A Outer variable trap example

---
+++ C#/OuterVariableTrap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Source: http://stackoverflow.com/questions/3416758
+
+namespace OuterVariableTrap
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var actions = new List<Action>();
+
+            for (var i = 0; i < 10; i++)
+                actions.Add(() => Console.Write($"{i} "));
+
+            // FIX:
+            //actions.Clear();
+            //for (var i = 0; i < 10; i++)
+            //{
+            //    int j = i;
+            //    actions.Add(() => Console.Write($"{j} "));
+            //}
+
+            actions.ForEach(a => a());
+            Console.ReadLine();
+        }
+    }
+}
