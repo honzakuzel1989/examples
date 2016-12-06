3781a77a8be92c8e8adb99bab6aae5ac0b662128
kuz1 <jan.kuzel@vf.cz>
2016-12-06 16:23:14 +0100

    A Exception example

---
+++ C#/TPL/AsyncAwait.Exception/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync()
+        {
+            Task t = ThrowExceptionAsync();
+            try
+            {
+                // the exception is raised when the task is awaited
+                await t;
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static async Task ThrowExceptionAsync()
+        {
+            await Task.Delay(1000);
+            throw new InvalidOperationException();
+        }
+    }
+}
