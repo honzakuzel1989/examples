133ad4f33fb15a9d5f4c375f6e5b8ea6afe70dd9
kuz1 <jan.kuzel@vf.cz>
2016-12-07 09:40:04 +0100

    A some examples
    * plinq example
    * attached task to parent example

---
+++ C#/TPL/PLINQ.1/Program.cs
@@ -0,0 +1,46 @@
+﻿#define PAR
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PLINQ._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Enumerable.Range(1, 1000);
+#if PAR
+            var start = DateTime.Now;
+            var powers = nums.AsParallel().Select(n => LongTermAction(n)).ToArray();
+            var end = DateTime.Now;
+            Console.WriteLine(end.Subtract(start));
+#else
+            // Someting around 50s
+            var start = DateTime.Now;
+            var powers = nums.Select(n => LongTermAction(n)).ToArray();
+            var end = DateTime.Now;
+            Console.WriteLine(end.Subtract(start));
+#endif
+            Console.ReadLine();
+        }
+
+        static int LongTermAction(int n)
+        {
+            Console.Write($"\rProcessing {n}\t\t\t\t\t");
+            // CPU bound
+            for (int i = 0; i < 100000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    int x = i * j + j;
+                }
+            }
+            return n * n;
+        }
+    }
+}
---
+++ C#/TPL/Task.AttachedToParent/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace TaskAttachedToParent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+            var t = Task.Factory.StartNew(() =>
+            {
+                WriteLine("Run parent..");
+                Do();
+                Thread.Sleep(2500);
+                WriteLine("Parent done..");
+            },
+                CancellationToken.None,
+                TaskCreationOptions.None,
+                TaskScheduler.Default);
+            // Wait for task and all attached tasks
+            t.Wait();
+            var end = DateTime.Now;
+
+            Console.WriteLine(end.Subtract(start));
+            Console.ReadLine();
+        }
+
+        static void Do()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                WriteLine("Run child..");
+                Thread.Sleep(5000);
+                WriteLine("Child done..");
+            },
+                CancellationToken.None,
+                TaskCreationOptions.AttachedToParent,
+                TaskScheduler.Default);
+
+            Task.Factory.StartNew(() =>
+            {
+                WriteLine("Run child..");
+                Thread.Sleep(7500);
+                WriteLine("Child done..");
+            },
+                CancellationToken.None,
+                TaskCreationOptions.AttachedToParent,
+                TaskScheduler.Default);
+        }
+    }
+}
