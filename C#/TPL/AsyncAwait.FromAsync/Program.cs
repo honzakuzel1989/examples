8b65f27f803b2d5e529390d57cf881e3d540b664
kuz1 <jan.kuzel@vf.cz>
2017-01-03 09:30:12 +0100

    A ImmutableList example with measuring of performance

---
+++ C#/TPL/AsyncAwait.FromAsync/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.FromAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+        }
+    }
+}
---
+++ C#/TPL/ListsPerformance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using System.Diagnostics;
+
+namespace ListsPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> l = null;
+            ImmutableList<int> il = null;
+
+            foreach (var N in new[] { 100, 1000, 10000, 100000, 1000000, 10000000 })
+            {
+                // init and fulfil list
+                l = new List<int>(Enumerable.Range(0, N));
+
+                // init and fulfil immutablelist
+                il = ImmutableList<int>.Empty;
+                il = il.InsertRange(0, Enumerable.Range(0, N));
+                
+                // O(N)
+                Stopwatch s = Stopwatch.StartNew();
+                foreach (var i in il)
+                { int j = i; }
+                s.Stop();
+                Console.WriteLine(s.ElapsedMilliseconds);
+
+                // O(log(N) * N)
+                s = Stopwatch.StartNew();
+                for (int i = 0; i < il.Count; i++)
+                { int j = il[i]; }
+                s.Stop();
+                Console.WriteLine(s.ElapsedMilliseconds);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
