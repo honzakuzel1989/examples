0eaf54d8df521f7b0f784d8d1c855df17e494fed
kuz1 <jan.kuzel@vf.cz>
2017-01-04 11:00:09 +0100

    A Cancellation examples

---
+++ C#/TPL/Cancellation.1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cancellation._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Do();
+
+            Console.ReadLine();
+        }
+
+        static async void Do()
+        {
+            DateTime startDt = DateTime.Now;
+
+            var cts = new CancellationTokenSource();
+            var ct = cts.Token;
+
+            // start task
+            var task = DoAsync(ct);
+
+            // wait
+            await Task.Delay(2500);
+
+            // cancel task after 2500ms
+            cts.Cancel();
+
+            try
+            {
+                // wait for task
+                await task;
+            }
+            catch (OperationCanceledException)
+            {
+                // task canceled before it completed
+                Console.WriteLine(nameof(OperationCanceledException));
+            }
+
+            DateTime endDt = DateTime.Now;
+
+            // something around 2500ms
+            Console.WriteLine(endDt.Subtract(startDt));
+        }
+
+        static async Task DoAsync(CancellationToken ct = default(CancellationToken))
+        {
+            await Task.Delay(5000, ct);
+        }
+    }
+}
---
+++ C#/TPL/Cancellation.2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cancellation._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Do();
+            Console.ReadLine();
+        }
+
+        static async void Do()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            var task = DoAsync(token);
+            await Task.Delay(5000);
+            cts.Cancel();
+
+            try { await task; }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Canceled");
+            }
+        }
+
+        private static Task DoAsync(CancellationToken token)
+        {
+            return Task.Run(() =>
+            {
+                for (int i = 0; i < int.MaxValue; i++)
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine(i + 1);
+                    token.ThrowIfCancellationRequested();
+                }
+            });
+        }
+    }
+}
