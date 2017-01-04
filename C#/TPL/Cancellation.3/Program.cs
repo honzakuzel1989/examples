216b5d4225c09ae51321ae565e1acca9b8a08e5d
kuz1 <jan.kuzel@vf.cz>
2017-01-04 13:11:58 +0100

    A cancellation by timeout example

---
+++ C#/TPL/Cancellation.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cancellation._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Do();
+            Console.ReadLine();
+        }
+
+        private static async void Do()
+        {
+            // cancel by timeot (after 2.5s)
+            var token = new CancellationTokenSource(2500).Token;
+
+            var startDt = DateTime.Now;
+
+            try { await Task.Delay(int.MaxValue, token); }
+            catch (OperationCanceledException) { }
+
+            Console.WriteLine(DateTime.Now.Subtract(startDt));
+        }
+    }
+}
