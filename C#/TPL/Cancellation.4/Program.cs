2006b10315c12bf82542dbde1996c6713571f877
kuz1 <jan.kuzel@vf.cz>
2017-01-04 15:11:11 +0100

    A cancellation by Register method (and by timeout)

---
+++ C#/TPL/Cancellation.4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cancellation._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Do();
+            Console.ReadLine();
+        }
+
+        private static async void Do()
+        {
+            try
+            {
+                await DownloadStringAsync(1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static async Task DownloadStringAsync(int timeout = 60)
+        {
+            using (var c = new WebClient())
+            {
+                using (new CancellationTokenSource(timeout * 1000).Token.Register(() => c.CancelAsync()))
+                {
+                    await c.DownloadFileTaskAsync(@"http://honzakuzel.eu/pic/gmail.png", "gmail.png");
+                    Console.WriteLine("Done");
+                }
+            }
+        }
+    }
+}
