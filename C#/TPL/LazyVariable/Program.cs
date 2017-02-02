5c0afe67abbe8122642bb057245d3c8daebb2425
kuz1 <jan.kuzel@vf.cz>
2017-02-02 15:17:17 +0100

    A Lazy initialization example

---
+++ C#/TPL/LazyVariable/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyVariable
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Foo f = new Foo();
+            Parallel.ForEach(Enumerable.Range(0, 10), (_) => Console.WriteLine(f.LazyInt.Value));
+            Parallel.ForEach(Enumerable.Range(0, 10), async (_) => Console.WriteLine(await f.LazyTaskInt.Value));
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class Foo
+    {
+        public Lazy<int> LazyInt { get; } = new Lazy<int>(
+            // The factory delegate is only executed once
+            () => { return 42; });
+
+        public Lazy<Task<int>> LazyTaskInt { get; } = new Lazy<Task<int>>(
+            // The factory delegate is only executed once
+            async () =>
+            {
+                await Task.Delay(3000).ConfigureAwait(false);
+                // ui or tpool or ... thread
+                return 42;
+            });
+        public Lazy<Task<int>> LazyTaskRunInt { get; } = new Lazy<Task<int>>(
+            // it may be better to allways execute the async. delegate on threadpool thread
+            () => Task.Run(
+                // The factory delegate is only executed once
+                async () =>
+                {
+                    await Task.Delay(3000);
+                    // tp thread
+                    return 42;
+                }
+            ));
+    }
+}
