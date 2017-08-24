661ab5cbd5dc176bba7daf6c0c97996a74fe762e
honzakuzel1989 <honza.kuzel.1989@gmail.com>
2017-08-24 18:34:12 +0200

    A Enumerated Types design pattern.

---
+++ C#/EnumeratedType/DayOfWeek.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratedType
+{
+    public static class DayOfWeekExt
+    {
+        public static string ToReadable(this IEnumerable<DayOfWeek> input)
+        {
+            return string.Join("\n", input);
+        }
+    }
+
+    public sealed class DayOfWeek
+    {
+        private static List<DayOfWeek> DaysOfWeek = new List<DayOfWeek>();
+
+        public int Order { get; private set; }
+        public string Name { get; private set; }
+
+        public static readonly DayOfWeek
+            /* You can set name from resources (language specific - czech in this case) */
+            MONDAY = new DayOfWeek(1, "Podnělí"),
+            THUESDAY = new DayOfWeek(2, "Úterý"),
+            WEDNESDAY = new DayOfWeek(3, "Středa"),
+            THURSDAY = new DayOfWeek(4, "Čtvrtek"),
+            FRIDAY = new DayOfWeek(5, "Pátek"),
+            SATURDAY = new DayOfWeek(6, "Soborta"),
+            SUNDAY = new DayOfWeek(7, "Neděle");
+
+        private DayOfWeek(int order, string name)
+        {
+            Order = order;
+            Name = name;
+
+            DaysOfWeek.Add(this);
+        }
+
+        public static IEnumerable<DayOfWeek> DaysOrder()
+        {
+            return DaysOfWeek.OrderBy(d => d.Order);
+        }
+
+        public static IEnumerable<DayOfWeek> DaysByName()
+        {
+            return DaysOfWeek.OrderBy(d => d.Name);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ C#/EnumeratedType/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratedType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DayOfWeek.DaysByName().ToReadable());
+            Console.WriteLine();
+            Console.WriteLine(DayOfWeek.DaysOrder().ToReadable());
+
+            Console.ReadLine();
+        }
+    }
+}
