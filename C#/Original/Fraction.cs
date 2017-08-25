0ff8b0e99d50f79424a73882ed5c5c7f066c2c0d
honzakuzel1989 <honza.kuzel.1989@gmail.com>
2017-08-25 12:13:51 +0200

    A Original weak design patter example.

---
+++ C#/Original/Fraction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Original
+{
+    class Fraction
+    {
+        private static Dictionary<string, WeakReference<Fraction>> usedFractions = new Dictionary<string, WeakReference<Fraction>>();
+
+        public static Fraction GetOrCreate(int numerator, int denominator)
+        {
+            Fraction fraction;
+            WeakReference<Fraction> result;
+            string key = $"{numerator}|{denominator}";
+
+            if (usedFractions.TryGetValue(key, out result))
+            {
+                if (!result.TryGetTarget(out fraction))
+                {
+                    fraction = new Fraction(numerator, denominator);
+                    usedFractions[key].SetTarget(fraction);
+                }
+            }
+            else
+            {
+                fraction = new Fraction(numerator, denominator);
+                usedFractions[key] = new WeakReference<Fraction>(fraction);
+            }
+
+            return fraction;
+        }
+
+        public int Numerator { get; private set; }
+        public int Denominator { get; private set; }
+
+        private Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+
+            Console.WriteLine($"Create new fraction {Numerator}|{Denominator}");
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}|{Denominator}";
+        }
+    }
+}
---
+++ C#/Original/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Original
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create new fractions
+            foreach (var i in Enumerable.Range(1, 100))
+            {
+                Fraction f = Fraction.GetOrCreate(1, i);
+            }
+
+            // --
+            GC.Collect();
+
+            // Recycle old fraction
+            foreach (var i in Enumerable.Range(1, 100))
+            {
+                Fraction f = Fraction.GetOrCreate(1, i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
