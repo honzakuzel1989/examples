f113ca7534214d34bd2c6a63ce8de783445f7738
honzakuzel1989 <honza.kuzel.1989@gmail.com>
2018-05-22 08:20:53 +0200

    A Cancellation by cts timeout in endless cycle

---
+++ C#/TPL/Cancellation.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cancellation._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            CancellationTokenSource cts = new CancellationTokenSource(2500);
+
+            while (true)
+            {
+                // Cancelled after 2500ms
+                if (cts.IsCancellationRequested)
+                    break;
+            }
+
+            Console.WriteLine(DateTime.Now.Subtract(start));
+            Console.ReadLine();
+        }
+    }
+}
