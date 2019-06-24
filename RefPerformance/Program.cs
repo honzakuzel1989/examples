171f0925b6d770b87a5010fd5daf7de66df65d6b
honzakuzel1989 <honza.kuzel.1989@gmail.com>
2019-06-24 14:23:46 +0200

    A ref performance example

---
+++ RefPerformance/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefPerformance
+{
+    class Program
+    {
+        const uint CNT = 100_000_000;
+
+        // 32B struct
+        struct S
+        {
+            public double a, b, c, d;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Marshal.SizeOf<S>());
+
+            DateTime start = DateTime.Now;
+            WithoutRef();
+            Console.WriteLine(DateTime.Now.Subtract(start));
+            WithRef();
+            Console.WriteLine(DateTime.Now.Subtract(start));
+            Console.ReadLine();
+        }
+
+        private static void WithoutRef()
+        {
+            var s = new S();
+            for (int i = 0; i < CNT; i++)
+            {
+                X(s);
+            }
+        }
+
+        private static S X(S s)
+        {
+            s.a = 'a';
+            s.b = 'b';
+            s.c = 'c';
+            s.d = 'd';
+
+            return s;
+        }
+
+        private static void WithRef()
+        {
+            var s = new S();
+            for (int i = 0; i < CNT; i++)
+            {
+                RefX(ref s);
+            }
+        }
+
+        private static void RefX(ref S s)
+        {
+            s.a = 'a';
+            s.b = 'b';
+            s.c = 'c';
+            s.d = 'd';
+        }
+    }
+}
