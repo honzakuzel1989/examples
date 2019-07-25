e2f75c65c1eb8e6fdb927773fc0c48906cc64892
honzakuzel1989 <honza.kuzel.1989@gmail.com>
2019-07-25 09:24:39 +0200

    A calling abstract method in ctor - do not do that

---
+++ C#/AdditionNullTrap/VirtualMethodInCtor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace VirtualMethodInCtor
+{
+    abstract class A
+    {
+        public A()
+        {
+            F();
+        }
+
+        protected abstract void F();
+    }
+
+    class B : A
+    {
+        string msg = "B.msg";
+
+        public B(string msg)
+        {
+            this.msg = msg;
+        }
+
+        protected override void F()
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _ = new B("Program");
+            Console.ReadLine();
+        }
+    }
+}
