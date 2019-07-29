4e6242a1eef861e536d305bdb15f493053ed7947
honzakuzel1989 <honza.kuzel.1989@gmail.com>
2019-07-29 15:34:14 +0200

    A AggressiveInlining example.

---
+++ C#/AggressiveInlining/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace AggressiveInlining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                new C().M();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            try
+            {
+                new C().N();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class C
+    {
+        public void M()
+        {
+            new D().M();
+        }
+
+        public void N()
+        {
+            new D().N();
+        }
+    }
+
+    class D
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void M()
+        {
+            throw new ApplicationException("D.M");
+        }
+
+        public void N()
+        {
+            throw new ApplicationException("E.M");
+        }
+    }
+}
