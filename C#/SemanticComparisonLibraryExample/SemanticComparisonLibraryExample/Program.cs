b96aed3bb5075d77bb65fae7c14b1b636cf499b4
Jan Kužel <jan.kuzel@alza.cz>
2020-03-25 22:51:20 +0100

    Likeness example

---
+++ C#/SemanticComparisonLibraryExample/SemanticComparisonLibraryExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SemanticComparison;
+
+namespace SemanticComparisonLibraryExample
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student();
+            st1.ID = 20;
+            st1.Name = "ligaoren";
+
+            Student st2 = new Student();
+            st2.ID = 20;
+            st2.Name = "ligaoren";
+
+            var expectedStudent = new Likeness<Student, Student>(st1);
+
+            Console.WriteLine(Equals(expectedStudent, st2));
+            Console.WriteLine(Equals(st1, st2));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/SemanticComparisonLibraryExample/SemanticComparisonLibraryExample/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace SemanticComparisonLibraryExample
+{
+    internal class Student
+    {
+        public Student()
+        {
+        }
+
+        public int ID { get; internal set; }
+        public string Name { get; internal set; }
+    }
+}
