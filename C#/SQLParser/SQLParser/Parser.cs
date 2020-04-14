58d9ca98bbfc22fea1913b7e5763e293a11f330a
Jan Kužel <jan.kuzel@alza.cz>
2020-04-14 18:29:35 +0200

    SQL parser example

---
+++ C#/SQLParser/SQLParser/Parser.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.SqlServer.Management.SqlParser.Parser;
+using System;
+using System.Collections.Generic;
+
+namespace SQLParser
+{
+    public class TokenInfo
+    {
+        public int Start { get; internal set; }
+        public int End { get; internal set; }
+        public bool IsPairMatch { get; internal set; }
+        public bool IsExecAutoParamHelp { get; internal set; }
+        public string Sql { get; internal set; }
+        public Tokens Token { get; internal set; }
+    }
+
+    public class Parser
+    {
+        public List<TokenInfo> ParseSql(string sql)
+        {
+            ParseOptions parseOptions = new ParseOptions();
+            Scanner scanner = new Scanner(parseOptions);
+
+            List<TokenInfo> tokens = new List<TokenInfo>();
+
+            scanner.SetSource(sql, offset: 0);
+
+            Tokens token;
+            int state = 0;
+            while ((token = (Tokens)scanner.GetNext(ref state, out int start, out int end, out bool isPairMatch, out bool isExecAutoParamHelp))
+                != Tokens.EOF)
+            {
+                TokenInfo tokenInfo = new TokenInfo()
+                {
+                    Start = start,
+                    End = end,
+                    IsPairMatch = isPairMatch,
+                    IsExecAutoParamHelp = isExecAutoParamHelp,
+                    Sql = sql.Substring(start, end - start + 1),
+                    Token = token,
+                };
+
+                tokens.Add(tokenInfo);
+            }
+
+            return tokens;
+        }
+    }
+}
---
+++ C#/SQLParser/SQLParser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SQLParser
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokens = new Parser()
+                .ParseSql("SELECT id, name, address FROM customer WHERE state LIKE '%Praha%' ORDER BY name ASC");
+
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token.Start);
+                Console.WriteLine(token.End);
+                Console.WriteLine(token.Sql);
+                Console.WriteLine(token.Token);
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
