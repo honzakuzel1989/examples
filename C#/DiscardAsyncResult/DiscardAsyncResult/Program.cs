1a4a367ed66babb540e6820d1d550d47056d0ce3
Jan Kužel <jan.kuzel@alza.cz>
2020-05-11 18:27:05 +0200

    Discard async result example.

---
+++ C#/DiscardAsyncResult/DiscardAsyncResult/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DiscardAsyncResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = DoSomethingAsync();
+            Console.ReadLine();
+        }
+
+        static async Task DoSomethingAsync()
+        {
+            Console.WriteLine("DoSomethingAsync()");
+            await Task.CompletedTask;
+        }
+    }
+}
