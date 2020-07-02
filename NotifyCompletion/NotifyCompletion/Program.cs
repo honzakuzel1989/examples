84938d8934d2a95fc1b0235c079de1957ba8db23
Jan Kužel <jan.kuzel@alza.cz>
2020-07-02 09:42:27 +0200

    NotifyCompletion example.

---
+++ NotifyCompletion/NotifyCompletion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace NotifyCompletion
+{
+    // source: https://tooslowexception.com/author/kkokosa/
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            bool t = true, f = false;
+
+            Console.WriteLine(await t);
+            Console.WriteLine(await f);
+
+            Console.ReadLine();
+        }
+    }
+
+    internal static class BoolExtensions
+    {
+        public static FunnyBoolAwaiter GetAwaiter(this bool value) 
+            => new FunnyBoolAwaiter(value);
+    }
+
+    internal class FunnyBoolAwaiter : INotifyCompletion
+    {
+        private readonly bool _value;
+        public FunnyBoolAwaiter(bool value) => _value = value;
+
+        public bool IsCompleted => true;
+        // Never called
+        public void OnCompleted(Action continuation) { } 
+        public bool GetResult() => !_value;
+    }
+}
