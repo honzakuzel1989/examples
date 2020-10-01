cbf7827f6adcba31961e40999855b217fc6ca57d
Jan Kužel <jan.kuzel@alza.cz>
2020-10-01 18:42:10 +0200

    Own tricky implementation of string

---
+++ C#/OwnString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OwnString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Compile error
+            // string i = 42;
+
+            // Implicit operator
+            String i = 42;
+
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+
+    class String
+    {
+        private readonly string _source;
+
+        private String(string source)
+        {
+            _source = source;
+        }
+
+        public static implicit operator String(int i) => new String(i.ToString());
+
+        public override string ToString()
+        {
+            return _source;
+        }
+    }
+}
