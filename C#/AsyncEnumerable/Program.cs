ef647b6a7d205ad6fcf4173e787e7ea20917448d
Jan Kužel <jan.kuzel@alza.cz>
2020-10-01 17:51:24 +0200

    Async enumerable example.

---
+++ C#/AsyncEnumerable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncEnumerable
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await foreach (var dataPoint in FetchIOTData())
+            {
+                Console.WriteLine(dataPoint);
+            }
+
+            Console.ReadLine();
+        }
+
+        static async IAsyncEnumerable<int> FetchIOTData()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(1000);
+                yield return i;
+            }
+        }
+    }
+}
